45b6a439c92c1059879e24b18d770f0e0effcf34
KoppalaSreenivasuluReddy <141857713+KoppalaSreenivasuluReddy@users.noreply.github.com>
2024-06-11 08:45:09 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,28 @@
+#data types 
+
+#strings
+
+print("hello"[0])
+
+print("156" + "854")
+
+#integer
+
+print(123 + 856)
+
+#float
+
+15.545
+
+#boolean 
+
+True
+False
+
+
+#num_char=len(input())
+#new_num_char=str(num_char)
+#print("your name contains" + new_num_char + "charcters")
+
+a=float(135)
+print(type(a))
